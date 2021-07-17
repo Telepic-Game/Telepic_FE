3caf87b2208f80c86ecdda1bad5742dec7d8b03c
Kris Litman <kris.d.litman@gmail.com>
2021-07-17 16:05:41 -0400

    Cleaned syntax in TestService method
    Co-authored by: 68141454+jordanfbeck0528@users.noreply.github.com

--- app/services/test_service.rb
+++ app/services/test_service.rb
@@ -3,7 +3,6 @@ class TestService < ApplicationController
 
   def self.clean_be_user_database
     conn = connect
-    # conn.delete("/api/v1/register/#{email}")
     conn.delete("/api/v1/test/users_all")
   end
 end
