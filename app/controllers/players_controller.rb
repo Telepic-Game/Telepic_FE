255fdeb3551f660b9e19e2574bb38e9a4fe01bbd
Jordan Beck <jordanfbeck@gmail.com>
2021-10-06 20:42:10 -0600

    Changes players controller create action to create player in front end. Player schema needs modification

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -4,16 +4,22 @@ class PlayersController < ApplicationController
   end
 
   def create
-    if params[:password].match(/(?=.{8,})(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!*()_-])(?=.*[0-9])/)
+    if (params[:password].match(/(?=.{8,})(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!*()_-])(?=.*[0-9])/)) && (params[:verify_email] == params[:email]) && (params[:password] == params[:password_confirmation])
     # Call the service to make the player in the BE db
-      can_register = RegistrationService.register_player(
+      # can_register = RegistrationService.register_player(
+      #   {
+      #     email: params[:email],
+      #     verify_email: params[:verify_email],
+      #     password: params[:password],
+      #     password_confirmation: params[:password_confirmation],
+      #   }
+      # )
+      can_register = Player.create(
         {
-          email: params[:email],
-          verify_email: params[:verify_email],
-          password: params[:password],
-          password_confirmation: params[:password_confirmation],
+          email: params[:email]
         }
       )
+      require "pry"; binding.pry
       if can_register[:response] && can_register[:permissions] == "registered"
       # Find player in the FE db if it was created
         player = Player.create(
