5cc5e7932950be20129147edd8f9e60da3f5dd7d
Kris Litman <kris.d.litman@gmail.com>
2021-12-05 18:18:41 -0500

    Building out guest path for opening a waiting room and testing different hotwire scenarios
    Co-authored by: 68141454+jordanfbeck0528@users.noreply.github.com

--- spec/features/waiting_room/hotwire_spec.rb
+++ spec/features/waiting_room/hotwire_spec.rb
@@ -13,7 +13,18 @@ RSpec.describe "Waiting Room - Hotwire Tests", type: :feature do
       fill_in :username, with: "Jordan Beck"
       click_button "Open Waiting Room"
 
+      expect(page.first('div#waiting_room_player').text).to eq('Jordan Beck')
       expect(current_path).to eq(waiting_room_path)
+
+      room_code = WaitingRoom.first.room_code
+
+      visit root_path
+      click_button "Join Game"
+      fill_in :username, with: "Kris Litman"
+      fill_in :room_code, with: room_code
+      click_button "Join Waiting Room"
+
+      # Continue Here!
     end
   end
 end
