3dc8caebf17b75801cb7a6f911e83e6ba41886f0
Jordan Beck <jordanfbeck@gmail.com>
2021-12-11 11:43:26 -0700

    Adds model tests for game
    56943408+krislitman@users.noreply.github.com

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,6 @@
 class Game < ApplicationRecord
   # validates :id, presence: true, uniqueness: true
-  has_one :stack, through: :game
-  has_many :cards, through: :stack
+  # has_one :stack, through: :game
+  # has_many :cards, through: :stack
   belongs_to :waiting_room
 end
